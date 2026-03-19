ad79d238dd5557e767ce503dac728295c8901579
xink <zyhh1611054604@gmail.com>
2026-03-20 00:47:58 +0800

    remove unused react-native-worklets/plugin from babel configuration

--- example/babel.config.js
+++ example/babel.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
-    'react-native-worklets/plugin',
     'react-native-reanimated/plugin',
     [
       'module-resolver',
